01434e48db3b0a2b82d10b8adea5fcdc903f61e1
Ksenia <97454789+Ksenia-Mesh@users.noreply.github.com>
2023-02-14 11:26:37 +0300

    Update RadioTest.java

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -4,3 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    void nextChannel() {
+        radio.setCurrentRadioStation(8);
+        radio.nextCurrentRadioStation();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+
+}
